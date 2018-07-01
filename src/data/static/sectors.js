1bd46f84f2d6a39517c043970ffeca2479f73ebb
Jacob Ramirez <mr.jacob.ramirez@gmail.com>
2018-07-01 00:44:39 -0500

    Adding last cities and all sectors

---
+++ src/data/static/sectors.js
@@ -0,0 +1,19 @@
+import type { Sector } from '../../domain/types/resources';
+
+const Sectors: Array<Sector> = [
+  { id: 1, label: 'Alimentos' },
+  { id: 2, label: 'Bebidas' },
+  { id: 3, label: 'Calzado' },
+  { id: 4, label: 'Cuero' },
+  { id: 5, label: 'Lácteos' },
+  { id: 6, label: 'Limpieza' },
+  { id: 7, label: 'Manualidades' },
+  { id: 8, label: 'Muebles' },
+  { id: 9, label: 'Panadería' },
+  { id: 10, label: 'Repostería' },
+  { id: 11, label: 'Ropa' },
+  { id: 12, label: 'Tecnología' },
+  { id: 13, label: 'Otros' },
+];
+
+export { Sectors };
