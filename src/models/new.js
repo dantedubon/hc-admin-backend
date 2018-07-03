aa5d5997d3872f9ebaf694b4568aab205e44b8cc
dante.dubon <dante.dubon@gmail.com>
2018-07-03 10:59:01 -0600

    adding fields to news

--- src/models/new.js
+++ src/models/new.js
@@ -1,31 +1,12 @@
 module.exports = function(sequelize: any, DataTypes: any) {
-  const New = sequelize.define(
-    "new",
-    {
-      companyId: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        references: {
-            model: "companies",
-            key: "id"
-        }
-      },
-      title:  { type: DataTypes.STRING, allowNull: false },
-      author:  { type: DataTypes.STRING, allowNull: false },
-      content:  { type: DataTypes.STRING, allowNull: false },
-      image:  { type: DataTypes.BLOB, allowNull: true },
-      isAccepted: { type: DataTypes.BOOLEAN, allowNull: false },
-      isDeleted: { type: DataTypes.BOOLEAN, allowNull: false },
-      createdAt:  { type: DataTypes.DATE, allowNull: true },
-      updatedAt:  { type: DataTypes.DATE, allowNull: true },
-    },
-    {
-      classMethods: {
-        associate(models) {
-          this.belongsTo(models.company);
-        }
-      }
-    }
-  );
+  const New = sequelize.define("new", {
+    title: { type: DataTypes.STRING, allowNull: false },
+    author: { type: DataTypes.STRING, allowNull: false },
+    content: { type: DataTypes.STRING, allowNull: false },
+    image: { type: DataTypes.BLOB, allowNull: true },
+    isDeleted: { type: DataTypes.BOOLEAN, allowNull: false },
+    createdAt: { type: DataTypes.DATE, allowNull: true },
+    updatedAt: { type: DataTypes.DATE, allowNull: true }
+  });
   return New;
 };
